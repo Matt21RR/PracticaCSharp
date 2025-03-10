22c1d55d74374391aeb321c7226638c79049572f
V1LLz <125107512+V1LLz@users.noreply.github.com>
2025-03-09 20:46:16 -0500

    Clase estatica para crear instancias de formularios de los distintos tipos de elementos

---
+++ src/FabricaFormularios.cs
@@ -0,0 +1,37 @@
+﻿using Actividad.view;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad.src
+{
+    public static class FabricaFormularios
+    {
+        public static UserControl CrearFormulario(string tipo, CentralDatos centralDatos, object elemento)
+        {
+            switch (tipo)
+            {
+                case "Estudiante":
+                    return new EstudianteForm(centralDatos, (Estudiante)elemento);
+                case "Persona":
+                    return new PersonaForm(centralDatos, (Persona)elemento);
+                case "Programa":
+                    return new ProgramaForm(centralDatos, (Programa)elemento);
+                case "Facultad":
+                    return new FacultadForm(centralDatos, (Facultad)elemento);
+                case "Profesor":
+                    return new ProfesorForm(centralDatos, (Profesor)elemento);
+                case "Curso":
+                    return new CursoForm(centralDatos, (Curso)elemento);
+                case "Inscripcion":
+                    return new InscripcionForm(centralDatos, (Inscripcion)elemento);
+                case "Curso con profesor":
+                    return new CursoProfesorForm(centralDatos, (CursoProfesor)elemento);
+                default:
+                    throw new ArgumentException("Tipo de formulario no válido.");
+            }
+        }
+    }
+}
